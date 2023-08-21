c0d3e161ce8edf5edf0e94c39615e6f6f31eebda
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-08-21 12:30:23 +0100

    Created new project for manipulating airport data from csv

---
+++ UsingAirportData/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UsingAirportData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
