713c63d220778bac799327f82fa72a2e55cde3e4
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-08-21 13:35:26 +0100

    Correctly reads local csv file to list obejct in memory

---
+++ UsingAirportData/Airport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace UsingAirportData
+{
+    internal class Airport
+    {
+        public int id { get; set; }
+        public string? ident { get; set; }
+        public string? type { get; set; }
+        public string? name { get; set; }
+        public decimal latitude_deg { get; set; }
+        public decimal longitude_deg { get; set; }
+        public int elevation_ft { get; set; }
+        public string? continent { get; set; }
+        public string? iso_country { get; set; }
+        public string? iso_region { get; set; }
+        public string? municipality { get; set; }
+        public string? scheduled_service { get; set; }
+        public string? gps_code { get; set; }
+        public string? iata_code { get; set; }
+        public string? local_code { get; set; }
+        public string? home_link { get; set; }
+        public string? wikipedia_link { get; set; }
+        public string? keywords { get; set; }
+
+        public override string ToString()
+        {
+            // Much nicer than building a long string
+            return JsonSerializer.Serialize<Airport>(this);    
+        }
+    }
+}
--- UsingAirportData/Program.cs
+++ UsingAirportData/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace UsingAirportData
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace UsingAirportData
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Airport> airportList;
+
+            // Locally stored CSV file containing airport data, source https://ourairports.com/data/
+            using (var reader = new StreamReader(@"C:\Users\luke\Desktop\CSharp Learning\Data\airports.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                // Register the new mapping to handle elevation_ft type error
+                csv.Context.RegisterClassMap<AirportMap>();
+                airportList = csv.GetRecords<Airport>().ToList();
+            }
+
+            foreach (var airport in airportList)
+            {
+                Console.WriteLine(airport);
+            }
+        }
+    }
+    internal class AirportMap : ClassMap<Airport>
+    {
+        public AirportMap()
+        {
+            // Default mapping for fields
+            AutoMap(CultureInfo.InvariantCulture);
+            // if elevation_ft field is empty, default to 0
+            Map(x => x.elevation_ft).Default(0);
         }
     }
 }
